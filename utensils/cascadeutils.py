3871864c8349111ddc9f908de5aad1011ab1a059
SpunkyHacker <tharunmuthu200@gmail.com>
2023-04-06 12:04:28 +0530

    done

--- utensils/cascadeutils.py
+++
@@ -1,5 +0,0 @@
-import os
-def generate_negative_description_file():
-    with open('neg.txt','w') as f:
-        for filename in os.listdir('negative'):
-            f.write('negative/' + filename + "\n")
