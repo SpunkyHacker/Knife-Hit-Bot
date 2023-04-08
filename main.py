c46396f4f818c636905de1b5dce5e70495822cb9
SpunkyHacker <tharunmuthu200@gmail.com>
2023-04-08 12:29:25 +0530

    improved some func but contains bugs

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ import random
 import win32api, win32con
 import linecache
 
-
-
-# X:  283 Y:  733 RGB: (  0, 216,  54)  r == 4 then its game 
-
-# play button x 480 y 731 0,178,71
+KNIFE = (270,190)
+RESET = (278,734) #if Red  == 0 then its reset button
+START = (414,198)
 
 def start():
-    click(480,731)
+    click(START)
 
-def click(x,y):
-    win32api.SetCursorPos((x,y))
+def click(coords):
+    print("clickedd")
+    win32api.SetCursorPos(coords)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, 0, 0)
     time.sleep(0.01)
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, 0, 0)
 
 def getRandomTime():
-    time =  random.randint(1,5)
+    time =  random.randint(1,3)
     print('got random time')
     return time
 
@@ -42,18 +41,10 @@ def getTimeInDatabase(linenum): #OK
     except ValueError:
         return 0
 
-def appendDatabase(linenum , time):
-    with open("database.txt") as file:
-        data = file.readlines()
-    try:
-        data[linenum - 1] = str(time) + "\n"
-        with open("database.txt", 'w') as file:
-            file.writelines(data)
-    except IndexError:
-        with open("database.txt",'a') as file:
-            file.write(str(time)+"\n")
+def appendDatabase(time):
+    with open("database.txt",'a') as file:
+        file.write(str(time)+"\n")
     print('appended time')
-    
 # def deleteData(line):
 #     print(line)
 #     with open("database.txt","r") as file:
@@ -72,36 +63,37 @@ def deleteData():
 
 
 def main():
-    countdown = 1
-    while keyboard.is_pressed('q') == False: # making sure we can exit
-        start() # for clicking the start button in the start of the game
-        while True: # comes here after geting reset button
-            countdown = 1 
-            while True: #illitrate until successful 
-                print(countdown)
-                if pyautogui.pixel(283, 733)[0] == 0: #checks for reset button
-                    print("countdown before deleting",countdown)
-                    click(283, 733) #clicks reset
-                    deleteData()
-                    break
-                flag = checkDatabase(countdown) #checks for pre loaded data
-                if flag: # gets the pre loaded data and clicks
-                    recordedTime = getTimeInDatabase(countdown)
-                    sleep(recordedTime)
-                    click(283, 733)
-                    if pixel(283, 733)[0] == 4: # check if we are in game  " that is the previous move was successful"
-                        countdown+=1
-                    else:
-                        print('try failed')
-                else: # generates randomly and clicks
-                    newTime = getRandomTime()
-                    sleep(newTime)
-                    click(283, 733)
-                    if pixel(283, 733)[0] == 4: # check if we are in game  " that is the previous move was successful"
-                        appendDatabase(countdown,newTime) #stores the new data
-                        countdown+=1
-                    else:
-                        print('try failed')
+    start()
+    sleep(1)
+    line = 1
+    while keyboard.is_pressed('q') == False:
+        flag = checkDatabase(line)
+        if flag == True:
+            time = getTimeInDatabase(line)
+            sleep(time)
+            click(KNIFE)
+            x,y=RESET
+            sleep(0.5)
+            if pyautogui.pixel(x,y)[0] == 0:
+                print("Data in database is wrong!")
+        elif flag == False:
+            time = getRandomTime()
+            sleep(time)
+            click(KNIFE)
+            x,y=RESET
+            sleep(0.5)
+            if pyautogui.pixel(x,y)[0] != 0:
+                appendDatabase( time)
+            else:
+                print('came in here')
+                break
+                click(RESET)
+        line+=1
+        
+
+
+
+
                     
 
 
